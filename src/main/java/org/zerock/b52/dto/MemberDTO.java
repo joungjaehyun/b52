9c34315390348ff74b9665f540e1c89882862a2d
gwanyongpark <joungjaehyun1008@gmail.com>
2023-06-26 16:32:42 +0900

    aa

--- src/main/java/org/zerock/b52/dto/MemberDTO.java
+++ src/main/java/org/zerock/b52/dto/MemberDTO.java
@@ -2,17 +2,19 @@ package org.zerock.b52.dto;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import lombok.Data;
 
 //User 를 상속받으면 UserDetails를 들어갈수있다.
 @Data
-public class MemberDTO extends User{
+public class MemberDTO extends User implements OAuth2User{
 
     private String mname;
 
@@ -26,6 +28,20 @@ public class MemberDTO extends User{
          .collect(Collectors.toList())
         );
         this.mname = mname;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+      // TODO Auto-generated method stub
+      //throw new UnsupportedOperationException("Unimplemented method 'getAttributes'");
+
+      return null;
+    }
+
+    @Override
+    public String getName() {
+
+      return this.getUsername();
     }
 
    
--- src/main/java/org/zerock/b52/security/CustomOAuth2UserService.java
+++ src/main/java/org/zerock/b52/security/CustomOAuth2UserService.java
@@ -1,18 +1,23 @@
 package org.zerock.b52.security;
 
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+import org.zerock.b52.dto.MemberDTO;
 
 import lombok.extern.log4j.Log4j2;
 
 @Service
 @Log4j2
-public class CustomOAuth2UserService extends DefaultOAuth2UserService{
-    
-    
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
 
@@ -20,7 +25,45 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService{
         log.info(userRequest);
         log.info("============================================");
 
-        return super.loadUser(userRequest);
+        ClientRegistration clientRegistration = userRequest.getClientRegistration();
+        String clientName = clientRegistration.getClientName();
+
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+        Map<String, Object> paramMap = oAuth2User.getAttributes();
+
+        String email = null;
+        // react에서는 직접 가공해서 함
+        switch (clientName) {
+            case "kakao":
+                email = getKakaoEmail(paramMap);
+                break;
+        }
+
+        log.info("===============================");
+        log.info(email);
+        log.info("===============================");
+        // 화면에서 사용자의 로그인한 정보를 찍는다 -> 타입이 달라지면 화면에서 처리할때 복잡해지므로 
+        // MemberDTO로 통일시켜주는것
+        MemberDTO memberDTO = new MemberDTO(email, "", "카카오사용자",List.of("USER"));
+
+        return memberDTO;
     }
-    
+
+    // 카카오에서 정보를 추출하는 함수
+    private String getKakaoEmail(Map<String, Object> paramMap) {
+
+        log.info("KAKAO-----------------------------------------");
+
+        Object value = paramMap.get("kakao_account");
+
+        log.info(value);
+
+        LinkedHashMap accountMap = (LinkedHashMap) value;
+
+        String email = (String) accountMap.get("email");
+
+        log.info("email..." + email);
+
+        return email;
+    }
 }
